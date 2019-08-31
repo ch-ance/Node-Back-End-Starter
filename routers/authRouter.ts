eca46375893659394aa75dea4d5f9491e5513a90
halo <cembreyfarquhar@gmail.com>
2019-08-30 23:33:24 -0500

    adds authRouter with type definitions

---
+++ routers/authRouter.ts
@@ -0,0 +1,53 @@
+import express = require("express");
+import bcrypt = require("bcryptjs");
+import jwt = require("jsonwebtoken");
+
+const secret = process.env.JWT_SECRET || "secret";
+
+const router = express.Router();
+
+// interface definitions
+
+interface Credentials {
+  username: string;
+  password: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+interface returnedUser extends Credentials {
+  id: number;
+}
+
+// Register Endpoint
+
+router.post("/register", async (req, res) => {
+  const user: Credentials = req.body;
+
+  const hash: string = hashPassword(user.password);
+
+  const hashedUser = { ...user, password: hash };
+
+  //   await Users.add
+});
+
+function hashPassword(pass: string) {
+  return bcrypt.hashSync(pass, 10);
+}
+
+function isValidHash(givenPass: string, realPass: string): boolean {
+  return bcrypt.compareSync(givenPass, realPass);
+}
+
+function generateToken(user: returnedUser): string {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
+}
